20281cf879c13cd964138e4592dfc93145c9baaf
RABIEMCH <mouhouchrabie2@gmail.com>
2023-12-12 22:01:25 +0000

    sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,60 @@
+
+ unys the average temp of 3 cities
+-- during July and August
+-- ordered by temperature desc
+
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;ittests for models/user.py.
+
+Unittest classes:
+    TestUserInstantiation
+    TestUserSave
+    TestUserToDict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.user import User
+
+
+class TestUserInstantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the User class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(User, type(User()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(User(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(User().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().updated_at))
+
+    def test_email_is_public_str(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_password_is_public_str(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name_is_public_str(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name_is_public_str(self):
+        self.assertEqual(str, type(User.last_name))
+
+    def test_two_users_unique_ids(self):
+        us1 = User()
+        us2 = User()
+        self.assertNotEqual(us1.id, us2.id)
+
